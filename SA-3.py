a6ca336f910b151fe475cda31d0c87f3ee5c4033
Rayane Yasmin Dos Santos <rayaneyasmind@gmail.com>
2026-06-18 10:44:26 -0300

    correção dos preços

--- SA-3.py
+++ SA-3.py
@@ -1,9 +1,9 @@
 estoque = [
-    [1, "Coração Humano", 3, "Câmara Fria A", 650.000,00],
-    [2, "Rim Humano", 8, "Câmara Fria B",27.000,00 ],
-    [3, "Útero Humano", 2, "Maleta Térmica 01", 500.000,00],
-    [4, "Fígado Humano", 6, "Câmara Fria C", 68.000,00],
-    [5, "Pulmão Humano", 4, "Câmara Fria D", 64.000,00]
+    [1, "Coração Humano", 3, "Câmara Fria A", 650000.00],
+    [2, "Rim Humano", 8, "Câmara Fria B",27000.00 ],
+    [3, "Útero Humano", 2, "Maleta Térmica 01", 500000.00],
+    [4, "Fígado Humano", 6, "Câmara Fria C", 680000.00],
+    [5, "Pulmão Humano", 4, "Câmara Fria D", 640000.00]
 ]
 
 proximo_id = 6
